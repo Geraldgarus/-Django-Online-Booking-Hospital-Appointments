3a56e13015ce04183aed2555222d2bc2218ee98e
Your Name <you@example.com>
2025-07-22 23:56:43 -0700

    Add Render URL to ALLOWED_HOSTS

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,12 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
+ALLOWED_HOSTS = [
+    'hospital-appoitment-booking.onrender.com',
+    '127.0.0.1',        # for local development
+    'localhost',
+]
+
 
 # Application definition
 
